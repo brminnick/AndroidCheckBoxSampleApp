58da33ab099f1b9e8387458440734161d05b5393
brminnick <brandon.r.minnick@gmail.com>
2016-08-25 18:06:46 -0700

    Updated NuGet Packages, Removed Unnecessary Using Statement,

--- CheckBoxSampleApp.UITest/Tests.cs
+++ CheckBoxSampleApp.UITest/Tests.cs
@@ -3,89 +3,90 @@
 using Xamarin.UITest;
 using Xamarin.UITest.Android;
 
-using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
-
 namespace CheckBoxSampleApp.UITest
 {
-    [TestFixture]
-    public class Tests
-    {
-        AndroidApp app;
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
 
-        [SetUp]
-        public void BeforeEachTest()
-        {
-            app = ConfigureApp
-                .Android
-                .InstalledApp("CheckBoxSampleApp.CheckBoxSampleApp")
-                .StartApp();
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp
+				.Android
+				.InstalledApp("CheckBoxSampleApp.CheckBoxSampleApp")
+				.PreferIdeSettings()
+				.StartApp();
 
-            app.Screenshot("First screen.");
-        }
+			app.Screenshot("First screen.");
+		}
 
-        [Test]
-        public void ReplTest()
-        {
-            app.Repl();
-        }
+		[Ignore]
+		[Test]
+		public void ReplTest()
+		{
+			app.Repl();
+		}
 
-        [TestCase("CheckBox1")]
-        [TestCase("CheckBox2")]
-        [TestCase("CheckBox3")]
-        [Test]
-        public void ToggleIndividualCheckBox(string textBoxContentDescription)
-        {
-            //Arrange
-            bool isCheckBoxChecked;
+		[TestCase("CheckBox1")]
+		[TestCase("CheckBox2")]
+		[TestCase("CheckBox3")]
+		[Test]
+		public void ToggleIndividualCheckBox(string textBoxContentDescription)
+		{
+			//Arrange
+			bool isCheckBoxChecked;
 
-            //Act
-            ToggleCheckBox(textBoxContentDescription);
+			//Act
+			ToggleCheckBox(textBoxContentDescription);
 
-            //Assert
-            isCheckBoxChecked = IsCheckBoxChecked(textBoxContentDescription);
-            Assert.IsTrue(isCheckBoxChecked, "The check box is not checked");
+			//Assert
+			isCheckBoxChecked = IsCheckBoxChecked(textBoxContentDescription);
+			Assert.IsTrue(isCheckBoxChecked, "The check box is not checked");
 
-            //Act
-            ToggleCheckBox(textBoxContentDescription);
+			//Act
+			ToggleCheckBox(textBoxContentDescription);
 
-            //Assert
-            isCheckBoxChecked = IsCheckBoxChecked(textBoxContentDescription);
-            Assert.IsFalse(isCheckBoxChecked, "The check box is not checked");
-        }
+			//Assert
+			isCheckBoxChecked = IsCheckBoxChecked(textBoxContentDescription);
+			Assert.IsFalse(isCheckBoxChecked, "The check box is not checked");
+		}
 
-        [Test]
-        public void ToggleAllCheckBoxes()
-        {
-            //Arrange
-            object[] isCheckBoxCheckedArray;
-            //Act
-            app.Query(x => x.Class("CheckBox").Invoke("performClick"));
+		[Test]
+		public void ToggleAllCheckBoxes()
+		{
+			//Arrange
+			object[] isCheckBoxCheckedArray;
 
-            //Assert
-            isCheckBoxCheckedArray = app.Query(x => x.Class("CheckBox").Invoke("isChecked"));
-            for (int i = 0; i < isCheckBoxCheckedArray.Length; i++)
-                Assert.IsTrue((bool)isCheckBoxCheckedArray[i], $"Check box {i} is not checked");
+			//Act
+			app.Query(x => x.Class("CheckBox").Invoke("performClick"));
 
-            //Act
-            app.Query(x => x.Class("CheckBox").Invoke("performClick"));
+			//Assert
+			isCheckBoxCheckedArray = app.Query(x => x.Class("CheckBox").Invoke("isChecked"));
+			for (int i = 0; i < isCheckBoxCheckedArray.Length; i++)
+				Assert.IsTrue((bool)isCheckBoxCheckedArray[i], $"Check box {i} is not checked");
 
-            //Assert
-            isCheckBoxCheckedArray = app.Query(x => x.Class("CheckBox").Invoke("isChecked"));
-            for (int i = 0; i < isCheckBoxCheckedArray.Length; i++)
-                Assert.False((bool)isCheckBoxCheckedArray[i], $"Check box {i} is checked");
-        }
+			//Act
+			app.Query(x => x.Class("CheckBox").Invoke("performClick"));
 
-        void ToggleCheckBox(string textBoxContentDescription)
-        {
-            app.Query(x => x.Marked(textBoxContentDescription).Invoke("performClick"));
-            app.Screenshot($"Toggled {textBoxContentDescription}");
-        }
+			//Assert
+			isCheckBoxCheckedArray = app.Query(x => x.Class("CheckBox").Invoke("isChecked"));
+			for (int i = 0; i < isCheckBoxCheckedArray.Length; i++)
+				Assert.False((bool)isCheckBoxCheckedArray[i], $"Check box {i} is checked");
+		}
 
-        bool IsCheckBoxChecked(string textBoxContentDescription)
-        {
-            return (bool)app.Query(x => x.Marked(textBoxContentDescription).Invoke("isChecked"))[0];
-        }
+		void ToggleCheckBox(string textBoxContentDescription)
+		{
+			app.Query(x => x.Marked(textBoxContentDescription).Invoke("performClick"));
+			app.Screenshot($"Toggled {textBoxContentDescription}");
+		}
 
-    }
+		bool IsCheckBoxChecked(string textBoxContentDescription)
+		{
+			return (bool)app.Query(x => x.Marked(textBoxContentDescription).Invoke("isChecked"))[0];
+		}
+
+	}
 }
 
