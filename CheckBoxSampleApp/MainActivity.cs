c8bb7ac8618ac4808a96421f109f7b9916524012
brminnick <brandon.r.minnick@gmail.com>
2017-05-19 09:29:48 -0700

    Added android:installLocation="auto"

--- CheckBoxSampleApp/MainActivity.cs
+++ CheckBoxSampleApp/MainActivity.cs
@@ -11,81 +11,81 @@ using CheckBoxSampleApp.Shared;
 
 namespace CheckBoxSampleApp
 {
-	[Activity(Label = "CheckBoxSampleApp", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Material.Light.DarkActionBar")]
-	public class MainActivity : Activity
-	{
-		Button _button1;
-		CheckBox _checkBox1, _checkBox2, _checkBox3;
-		TextView _textView1;
+    [Activity(Label = "CheckBoxSampleApp", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Material.Light.DarkActionBar")]
+    public class MainActivity : Activity
+    {
+        Button _button1;
+        CheckBox _checkBox1, _checkBox2, _checkBox3;
+        TextView _textView1;
 
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
-			SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
 
-			_button1 = FindViewById<Button>(Resource.Id.Button1);
-			_checkBox1 = FindViewById<CheckBox>(Resource.Id.CheckBox1);
-			_checkBox2 = FindViewById<CheckBox>(Resource.Id.CheckBox2);
-			_checkBox3 = FindViewById<CheckBox>(Resource.Id.CheckBox3);
-			_textView1 = FindViewById<TextView>(Resource.Id.TextView1);
+            _button1 = FindViewById<Button>(Resource.Id.Button1);
+            _checkBox1 = FindViewById<CheckBox>(Resource.Id.CheckBox1);
+            _checkBox2 = FindViewById<CheckBox>(Resource.Id.CheckBox2);
+            _checkBox3 = FindViewById<CheckBox>(Resource.Id.CheckBox3);
+            _textView1 = FindViewById<TextView>(Resource.Id.TextView1);
 
-			_button1.Click += HandleButtonClick;
-			_checkBox1.Click += HandleCheckBoxClick;
-			_checkBox2.Click += HandleCheckBoxClick;
-			_checkBox3.Click += HandleCheckBoxClick;
+            _button1.Click += HandleButtonClick;
+            _checkBox1.Click += HandleCheckBoxClick;
+            _checkBox2.Click += HandleCheckBoxClick;
+            _checkBox3.Click += HandleCheckBoxClick;
 
-			AssignContentDescriptions();
-		}
+            AssignContentDescriptions();
+        }
 
-		void AssignContentDescriptions()
-		{
-			_button1.ContentDescription = AutomationConstants.Button1;
-			_checkBox1.ContentDescription = AutomationConstants.CheckBox1;
-			_checkBox2.ContentDescription = AutomationConstants.CheckBox2;
-			_checkBox3.ContentDescription = AutomationConstants.CheckBox3;
-			_textView1.ContentDescription = AutomationConstants.TextView1;
-		}
+        void AssignContentDescriptions()
+        {
+            _button1.ContentDescription = AutomationConstants.Button1;
+            _checkBox1.ContentDescription = AutomationConstants.CheckBox1;
+            _checkBox2.ContentDescription = AutomationConstants.CheckBox2;
+            _checkBox3.ContentDescription = AutomationConstants.CheckBox3;
+            _textView1.ContentDescription = AutomationConstants.TextView1;
+        }
 
-		void HandleCheckBoxClick(object sender, EventArgs e)
-		{
-			var checkBox = sender as CheckBox;
+        void HandleCheckBoxClick(object sender, EventArgs e)
+        {
+            var checkBox = sender as CheckBox;
 
-			if (checkBox?.ContentDescription == null)
-				return;
+            if (checkBox?.ContentDescription == null)
+                return;
 
-			var checkBoxNumber = int.Parse(checkBox?.ContentDescription.Substring(8));
-			checkBox.Text = checkBox.Checked ? $"Check Box {checkBoxNumber} is Checked" : $"Check Box {checkBoxNumber} is Unchecked";
-		}
+            var checkBoxNumber = int.Parse(checkBox?.ContentDescription.Substring(8));
+            checkBox.Text = checkBox.Checked ? $"Check Box {checkBoxNumber} is Checked" : $"Check Box {checkBoxNumber} is Unchecked";
+        }
 
-		void HandleButtonClick(object sender, EventArgs e)
-		{
-			int numberOfCheckBoxesChecked = 0;
+        void HandleButtonClick(object sender, EventArgs e)
+        {
+            int numberOfCheckBoxesChecked = 0;
 
-			if (_checkBox1.Checked)
-				numberOfCheckBoxesChecked++;
-			if (_checkBox2.Checked)
-				numberOfCheckBoxesChecked++;
-			if (_checkBox3.Checked)
-				numberOfCheckBoxesChecked++;
+            if (_checkBox1.Checked)
+                numberOfCheckBoxesChecked++;
+            if (_checkBox2.Checked)
+                numberOfCheckBoxesChecked++;
+            if (_checkBox3.Checked)
+                numberOfCheckBoxesChecked++;
 
-			if (numberOfCheckBoxesChecked == 1)
-				_textView1.Text = $"{numberOfCheckBoxesChecked} Check Box Is Checked";
-			else
-				_textView1.Text = $"{numberOfCheckBoxesChecked} Check Boxes Are Checked";
-		}
+            if (numberOfCheckBoxesChecked == 1)
+                _textView1.Text = $"{numberOfCheckBoxesChecked} Check Box Is Checked";
+            else
+                _textView1.Text = $"{numberOfCheckBoxesChecked} Check Boxes Are Checked";
+        }
 
-		#region Xamarin UITest Backdoor Methods
+        #region Xamarin UITest Backdoor Methods
 #if DEBUG
-		[Export("GetColorAsInt")]
-		public string GetColorAsInt(string colorName)
-		{
-			int colorAsInt = Color.ParseColor(colorName.ToString());
+        [Export("GetColorAsInt")]
+        public string GetColorAsInt(string colorName)
+        {
+            int colorAsInt = Color.ParseColor(colorName);
 
-			return colorAsInt.ToString();
-		}
+            return colorAsInt.ToString();
+        }
 #endif
-		#endregion
-	}
+        #endregion
+    }
 }
 
